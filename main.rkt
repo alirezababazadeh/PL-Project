4b367be8f0e0cf8b398c11eb82e130b235a63f1f
seyyedAlirezaGhazanfari <alireza79.ghazanfari@gmail.com>
2024-01-28 21:12:23 +0330

    add some value ofs

--- main.rkt
+++ main.rkt
@@ -10,30 +10,71 @@
     )
   )
 
+(define-datatype answer answer?
+  (a-ans
+   (value expval?)
+   (msg symbol?)
+   (env environment?)))
+
+
+(define answer-val
+  (lambda (ans)
+    (cases answer ans
+      (a-ans (val msg env) val))))
+
+(define extract-env
+  (lambda (ans)
+    (cases answer ans
+      (a-ans (val msg env) env))))
+
+(define ret-ans?
+  (lambda (ans)
+    (cases answer ans
+      (a-ans (val msg env) (eqv? msg 'return)))))
+
+(define continue-ans?
+  (lambda (ans)
+    (cases answer ans
+      (a-ans (val msg env) (eqv? msg 'continue)))))
+
+(define break-ans?
+  (lambda (ans)
+    (cases answer ans
+      (a-ans (val msg env) (eqv? msg 'break)))))
+
+(define-datatype thunk thunk?
+  (a-thunk
+   (exp expression?)
+   (env environment?)))
+
 ; Program
 (define value-of-program
   (lambda (pgm)
     (cases program pgm
       (a-program (stats)
-                (value-of stats (init-env))))
+                 (value-of-stats stats (init-env))))
     )
   )
 
+; extract-env
+; break-ans? - ret-ans? - continue-ans?
 ; Statements
-(define value-of-statements
+(define value-of-stats
   (lambda (stats env)
     (cases statements stats
       (a-statement (stat)
-                   (value-of-statement stat env)
+                   (value-of-stmt stat env)
                    )
       (cum-statements (stats stat)
                       (let ((answer-stats (value-of-statements stats env)))
-                        
+                        (if (not (or (ret-ans? answer-stats) (break-ans? answer-stats) (continue-ans? answer-stats)))
+                            (value-of-stmt stat (extract-env answer-stats))
+                            answer-stats)
                         )
-                      )
                       )
       )
     )
+  )
 
 ; Statement
 (define value-of-stmt
@@ -50,9 +91,17 @@
       (assign-stmt (assign) (value-of-assignment assign env))
       (glob-stmt (glob) (value-of-global-stmt glob env))
       (ret-stmt (ret) (value-of-return-stmt ret env))
-      (pass-stmt () ())
-      (beak-stmt () ())
-      (continue-stmt () ())
-      (simple-print-stmt () ())
+      (pass-stmt () (a-ans (a-none) '- env))
+      (beak-stmt () (a-ans (a-none) 'break env))
+      (continue-stmt () (a-ans (a-none) 'continue env))
       )
     ))
+
+; Assignment-stmt
+(define value-of-assignment
+  (lambda (stat env)
+    (cases assignment-stmt stat
+      (a-assign-stmt (identifier expr) (a-ans (a-none) '- (extend-env env identifier (a-thunk expr (copy-of-env env)))))
+      )
+    )
+  )
