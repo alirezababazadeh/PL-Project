5bf7c59974ce89b6d106a0b4dfadc6a98929dfab
seyyedAlirezaGhazanfari <alireza79.ghazanfari@gmail.com>
2024-01-28 21:18:02 +0330

    add return value of

--- main.rkt
+++ main.rkt
@@ -90,7 +90,7 @@
     (cases simple-stmt stat
       (assign-stmt (assign) (value-of-assignment assign env))
       (glob-stmt (glob) (value-of-global-stmt glob env))
-      (ret-stmt (ret) (value-of-return-stmt ret env))
+      (ret-stmt (ret) (value-of-return ret env))
       (pass-stmt () (a-ans (a-none) '- env))
       (beak-stmt () (a-ans (a-none) 'break env))
       (continue-stmt () (a-ans (a-none) 'continue env))
@@ -102,6 +102,18 @@
   (lambda (stat env)
     (cases assignment-stmt stat
       (a-assign-stmt (identifier expr) (a-ans (a-none) '- (extend-env env identifier (a-thunk expr (copy-of-env env)))))
+      )
+    )
+  )
+
+; Return-stmt
+(define value-of-return
+  (lambda (stat env)
+    (cases return-stmt stat
+      (return-void-stmt () (a-ans (a-none) 'return env))
+      (return-exp-stmt (expr) (let
+                                  ((resp (value-of-expression expr env)))
+                                (a-ans (ans-val resp) 'return (extract-env resp))))
       )
     )
   )
