38c98c84ce3fdc806f1eba7f9c8a39c2d13ccf7a
masihbr <masihbr@gmail.com>
2024-01-28 16:09:50 +0330

    Use evaluate to evaluate and run program

--- parse.rkt
+++ parse.rkt
@@ -3,12 +3,12 @@
 (require "passes/parser.rkt")
 (require "passes/lexer.rkt")
 
-(define (parse-scan prog-string)
+(define (lex&parse prog-string)
   (python-parser (lex-this prog-string))
   )
 
-(define (evaluate file-name)
-  (parse-scan (string-join (file->lines file-name)))
+(define (scan&parse file-name)
+  (lex&parse (string-join (file->lines file-name)))
   )
 
 (provide (all-defined-out))
