a50a1c67a8bf92f68d2ea7001ff75758ce7d56be
seyyedAlirezaGhazanfari <alireza79.ghazanfari@gmail.com>
2024-01-30 02:39:06 +0330

    redesign and add some rules

--- grammar.rkt
+++ grammar.rkt
@@ -7,344 +7,344 @@
 ; Program → Statements EOF
 (define-datatype program program?
   (a-program
-   (stats statements?)
-   )
-  )
+   (stats statements?)))
+   
+  
 
 ; Statements → Statement ';' | Statements Statement ';'
 (define-datatype statements statements?
   (a-statement
-   (stat statement?)
-   )
+   (stat statement?))
+   
   (cum-statements
    (stats statements?)
-   (stat statement?)
-   )
-  )
+   (stat statement?)))
+   
+  
 
 ; Statement → Compound_stmt | Simple_stmt
 (define-datatype statement statement?
   (a-compound-stmt
-   (cmp-stmt compound-stmt?)
-   )
+   (cmp-stmt compound-stmt?))
+   
   (a-simple-stmt
-   (sim-stmt simple-stmt?)
-   )
-  )
+   (sim-stmt simple-stmt?)))
+   
+  
 
 ; Simple_stmt → Assignment | Global_stmt | Return_stmt
 ; Simple_stmt → 'pass' | 'break' | 'continue'
 ; Simple_stmt → 'print' '()' | 'print' '('Arguments')'
 (define-datatype simple-stmt simple-stmt?
   (assign-stmt
-   (assign assignment-stmt?)
-   )
+   (assign assignment-stmt?))
+   
   (glob-stmt
-   (glob global-stmt?)
-   )
+   (glob global-stmt?))
+   
   (ret-stmt
-   (ret return-stmt?)
-   )
+   (ret return-stmt?))
+   
   (pass-stmt)
   (break-stmt)
   (continue-stmt)
   (simple-print-stmt)
   (print-stmt
-   (args arguments?)
-   )
-  )
+   (args arguments?)))
+   
+  
 
 ; Compound_stmt → Function_def | If_stmt | For_stmt
 (define-datatype compound-stmt compound-stmt?
   (cmp-function-def
-   (func-def function-def?)
-   )
+   (func-def function-def?))
+   
   (cmp-if-stmt
-   (if-stmt if-stmt?)
-   )
+   (if-stmt if-stmt?))
+   
   (cmp-for-stmt
-   (for-stmt for-stmt?)
-   )
-  )
+   (for-stmt for-stmt?)))
+   
+  
 
 ; Assignment → ID '=' Expression
 (define-datatype assignment-stmt assignment-stmt?
   (a-assign-stmt
    (identifier symbol?)
-   (expr expression?)
-   )
-  )
+   (expr expression?)))
+   
+  
 
 ; Return_stmt → 'return' | 'return' Expression
 (define-datatype return-stmt return-stmt?
   (return-void-stmt)
   (return-exp-stmt
-   (expr expression?)
-   )
-  )
+   (expr expression?)))
+   
+  
 
 ; Global_stmt → 'global' ID
 (define-datatype global-stmt global-stmt?
   (a-global-stmt
-   (identifier symbol?)
-   )
-  )
+   (identifier symbol?)))
+   
+  
 
 ; Function_def → 'def' ID '(' Params ')' ':' Statements | 'def' ID '():' Statements
 (define-datatype function-def function-def?
   (func-def-with-params
    (identifier symbol?)
    (params params?)
-   (stats statements?)
-   )
+   (stats statements?))
+   
   (func-def-no-params
    (identifier symbol?)
-   (stats statements?)
-   )
-  )
+   (stats statements?)))
+   
+  
 
 ; Params → Param_with_default | Params ',' Param_with_default
 (define-datatype params params?
   (empty-param)
   (func-params 
    (param param?) 
-   (rest-params params?)
-   )
-  )
+   (rest-params params?)))
+   
+  
 
 ; Param_with_default → ID '=' Expression
 (define-datatype param param?
   (with_default
    (identifier string?)
-   (expr expression?)
-   )
-  )
+   (expr expression?)))
+   
+  
 
 ; If_stmt → 'if' Expression ':' Statements Else_block
 (define-datatype if-stmt if-stmt?
   (a-if-stmt
    (cond-expr expression?)
    (if-stats statements?)
-   (else-block else-block?)
-   )
-  )
+   (else-block else-block?)))
+   
+  
 
 ; Else_block → 'else' ':' Statements
 (define-datatype else-block else-block?
   (a-else-block
-   (else-stats statements?)
-   )
-  )
+   (else-stats statements?)))
+   
+  
 
 ; For_stmt → 'for' ID 'in' Expression ':' Statements
 (define-datatype for-stmt for-stmt?
   (a-for-stmt
    (iter symbol?)
    (expr expression?)
-   (stats statements?)
-   )
-  )
+   (stats statements?)))
+   
+  
 
 ; Expression → Disjunction
 (define-datatype expression expression?
   (disjunct-expression
-   (disjunction disjunction?)
-   )
-  )
+   (disjunc disjunction?)))
+   
+  
 
 ; Disjunction → Conjunction | Disjunction 'or' Conjunction
 (define-datatype disjunction disjunction?
   (a-disjunction
-   (conjunction conjunction?)
-   )
+   (conjunc conjunction?))
+   
   (cum-disjunction
-   (disjunction disjunction?)
-   (conjunction conjunction?)
-   )
-  )
+   (disjunc disjunction?)
+   (conjunc conjunction?)))
+   
+  
 
 ; Conjunction → Inversion | Conjunction 'and' Inversion
 (define-datatype conjunction conjunction?
   (a-conjunction
-   (inversion inversion?)
-   )
+   (invers inversion?))
+   
   (cum-conjunction
-   (conjunction conjunction?)
-   (inversion inversion?)
-   )
-  )
+   (conjunc conjunction?)
+   (invers inversion?)))
+   
+  
 
 ; Inversion → 'not' Inversion | Comparison
 (define-datatype inversion inversion?
   (not-of-inversion
-   (inversion inversion?)
-   )
+   (invers inversion?))
+   
   (a-comparison
-   (comparsion comparison?)
-   )
-  )
+   (comparsion comparison?)))
+   
+  
 
 ; Comparison → Eq_Sum | Lt_Sum | Gt_Sum | Sum
 (define-datatype comparison comparison?
   (eq-comp
-   (eq-sum eq-sum?)
-   )
+   (eq-sum eq-sum?))
+   
   (lt-comp
-   (lt-sum lt-sum?)
-   )
+   (lt-sum lt-sum?))
+   
   (gt-comp
-   (gt-sum gt-sum?)
-   )
+   (gt-sum gt-sum?))
+   
   (sum-comp
-   (sum sum?)
-   )
-  )
+   (sum sum?)))
+   
+  
 
 ; Eq_Sum → Sum '==' Sum
 (define-datatype eq-sum eq-sum?
   (a-eq-sum
    (sum1 sum?)
-   (sum2 sum?)
-   )
-  )
+   (sum2 sum?)))
+   
+  
 
 ; Lt_Sum → sum '<' Sum
 (define-datatype lt-sum lt-sum?
   (a-lt-sum
    (sum1 sum?)
-   (sum2 sum?)
-   )
-  )
+   (sum2 sum?)))
+   
+  
 
 ; Gt_Sum → sum '>' Sum
 (define-datatype gt-sum gt-sum?
   (a-gt-sum
    (sum1 sum?)
-   (sum2 sum?)
-   )
-  )
+   (sum2 sum?)))
+   
+  
 
 ; Sum → Sum '+' Term | Sum '-' Term | Term
 (define-datatype sum sum?
   (plus-sum
    (sum sum?)
-   (term term?)
-   )
+   (term term?))
+   
   (minus-sum
    (sum sum?)
-   (term term?)
-   )
+   (term term?))
+   
   (term-sum
-   (term term?)
-   )
-  )
+   (term term?)))
+   
+  
 
 ; Term → Term '∗' Factor | Term '/' F actor | Factor
 (define-datatype term term?
   (mult-term
    (term term?)
-   (factor factor?)
-   )
+   (factor factor?))
+   
   (div-term
    (term term?)
-   (factor factor?)
-   )
+   (factor factor?))
+   
   (factor-term
-   (factor factor?)
-   )
-  )
+   (factor factor?)))
+   
+  
 
 ; Factor → '+' Power | '-' Power | Power
 (define-datatype factor factor?
   (pos-factor
-   (power power?)
-   )
+   (power power?))
+   
   (neg-factor
-   (power power?)
-   )
+   (power power?))
+   
   (power-factor
-   (power power?)
-   )
-  )
+   (power power?)))
+   
+  
 
 ; Power → Atom '∗ ∗' Factor | Primary
 (define-datatype power power?
   (atom-powered
    (atom atom?)
-   (factor factor?)
-   )
+   (factor factor?))
+   
   (a-primary
-   (primary primary?)
-   )
-  )
+   (primary primary?)))
+   
+  
 
 ; Primary → Atom | Primary '[' Expression ']' | Primary '()' 
 ; Primary → Primary '(' Arguments ')'
 (define-datatype primary primary?
   (a-atom
-   (atom atom?)
-   )
+   (atom atom?))
+   
   (arr-access
    (primary primary?)
-   (expr expression?)
-   )
+   (expr expression?))
+   
   (func-call-no-arg
    (primary primary?))
   (func-call-with-args
    (primary primary?)
-   (arguments arguments?)
-   )
-  )
+   (arguments arguments?)))
+   
+  
 
 ; Arguments → Expression | Arguments ',' Expression
 (define-datatype arguments arguments? 
   (arg-expression
-   (expr expression?)
-   )
+   (expr expression?))
+   
   (args-expression
    (args arguments?)
-   (expr expression?)
-   )
-  )
+   (expr expression?)))
+   
+  
 
 (define-datatype none none?
-  (none-stmt)
-  )
+  (none-stmt))
+  
 
 ; Atom → ID | 'True' | 'False' | 'None' | NUMBER | List
 (define-datatype atom atom?
   (a-id
-   (identifier symbol?)
-   )
+   (identifier symbol?))
+   
   (a-bool
-   (bool boolean?)
-   )
+   (bool boolean?))
+   
   (a-none
-   (none none?)
-   )
+   (none none?))
+   
   (a-num
-   (num number?)
-   )
+   (num number?))
+   
   (a-list
-   (py-list python-list?)
-   )
-  )
+   (py-list python-list?)))
+   
+  
 
 ; List → '[' Expressions ']' | '[]'
 (define-datatype python-list python-list?
   (filled-list
-   (exprs expressions?)
-   )
-  (empty-list)
-  )
+   (exprs expressions?))
+   
+  (empty-list))
+  
 
 ; Expressions → Expressions ',' Expression | Expression
 (define-datatype expressions expressions?
   (cum-expression
    (exprs expressions?)
-   (expr expression?)
-   )
+   (expr expression?))
+   
   (a-expression
-   (expr expression?)
-   )
-  )
+   (expr expression?)))
+   
+  
--- main.rkt
+++ main.rkt
@@ -9,9 +9,9 @@
 
 (define evaluate
   (lambda (input-string)
-    (value-of-program (scan&parse input-string))
-    )
-  )
+    (value-of-program (scan&parse input-string))))
+    
+  
 
 (define-datatype expval expval?
   (num-val
@@ -19,12 +19,12 @@
   (bool-val
    (bool boolean?))
   (none-val
-   (n none?)
-   )
+   (n none?))
+   
   (func-val
-   (f func?)
-   )
-  )
+   (f func?)))
+   
+  
 
 (define-datatype answer answer?
   (a-ans
@@ -67,37 +67,37 @@
   (lambda (pgm)
     (cases program pgm
       (a-program (stats)
-                 (value-of-stats stats (gb-sc-new))))
-    )
-  )
+                 (value-of-stats stats (gb-sc-new))))))
+    
+  
 
 ; Statements
 (define value-of-stats
   (lambda (stats sc)
     (cases statements stats
       (a-statement (stat)
-                   (value-of-stmt stat sc)
-                   )
+                   (value-of-stmt stat sc))
+                   
       (cum-statements (stats stat)
                      (let ((answer-stats (value-of-stats stats sc)))
                         (if (not (or (ret-ans? answer-stats) (break-ans? answer-stats) (continue-ans? answer-stats)))
                             (value-of-stmt stat (extract-sc answer-stats))
-                            answer-stats)
-                        )
-                      )
-      )
-    )
-  )
+                            answer-stats))))))
+                        
+                      
+      
+    
+  
 
 ; Statement
 (define value-of-stmt
   (lambda (stat sc)
     (cases statement stat
       (a-compound-stmt (cmp-stat) (value-of-compound-stmt cmp-stat sc))
-      (a-simple-stmt (sim-stmt) (value-of-simple-stmt sim-stmt sc))
-      )
-    )
-  )
+      (a-simple-stmt (sim-stmt) (value-of-simple-stmt sim-stmt sc)))))
+      
+    
+  
 
 ; Simple-stmt
 (define value-of-simple-stmt
@@ -110,19 +110,19 @@
       (break-stmt () (a-ans (a-none) 'break sc))
       (continue-stmt () (a-ans (a-none) 'continue sc))
       (simple-print-stmt () (display ""))
-      (print-stmt (args) (value-of-print-stmt args))
-      )
-    )
-  )
+      (print-stmt (args) (value-of-print-stmt args)))))
+      
+    
+  
 
 ; Assignment-stmt
 (define value-of-assignment
   (lambda (stat sc)
     (cases assignment-stmt stat
-      (a-assign-stmt (identifier expr) (a-ans (a-none) '- (extend-sc sc identifier (a-thunk expr (copy-of-sc sc)))))
-      )
-    )
-  )
+      (a-assign-stmt (identifier expr) (a-ans (a-none) '- (extend-sc sc identifier (a-thunk expr (copy-of-sc sc))))))))
+      
+    
+  
 
 ; Return-stmt
 (define value-of-return
@@ -131,19 +131,19 @@
       (return-void-stmt () (a-ans (a-none) 'return sc))
       (return-exp-stmt (expr) (let
                                   ((resp (value-of-expression expr sc)))
-                                (a-ans (ans-val resp) 'return (extract-sc resp))))
-      )
-    )
-  )
+                                (a-ans (ans-val resp) 'return (extract-sc resp)))))))
+      
+    
+  
 
 ; Global-stmt
 (define value-of-global-stmt
   (lambda (stat sc)
     (cases global-stmt stat
-      (a-global-stmt (identifier) (a-ans (a-none) '- (add-global-var sc identifier)))
-      )
-    )
-  )
+      (a-global-stmt (identifier) (a-ans (a-none) '- (add-global-var sc identifier))))))
+      
+    
+  
 
 ; Compound-stmt
 (define value-of-compound-stmt
@@ -151,10 +151,10 @@
     (cases compound-stmt stat
       (cmp-function-def (func-def) (value-of-func-def func-def sc))
       (cmp-if-stmt (if-stmt) (value-of-if-stmt if-stmt sc))
-      (cmp-for-stmt (for-stmt) (value-of-for-stmt for-stmt sc))
-      )
-    )
-  )
+      (cmp-for-stmt (for-stmt) (value-of-for-stmt for-stmt sc)))))
+      
+    
+  
 
 ; Function_def
 (define-datatype func func?
@@ -170,18 +170,18 @@
       (func-def-with-params
        (identifier params stats)
        (let ((f (a-func identifier params stats (lc-sc-new scope))))
-         (a-ans (a-none) '- (extend-sc sc identifier f))
-         )
-       )
+         (a-ans (a-none) '- (extend-sc sc identifier f))))
+         
+       
       (func-def-no-params 
        (identifier stats)
        (let ((f (a-func identifier (a-none) sts (lc-sc-new scope))))
-         (a-ans (a-none) '- (extend-sc sc identifier f))
-         )
-       )
-      )
-    )
-  )
+         (a-ans (a-none) '- (extend-sc sc identifier f)))))))
+         
+       
+      
+    
+  
 
 ; If_stmt
 (define value-of-if-stmt
@@ -192,23 +192,24 @@
        (let ((resp (value-of-expression cond-expr sc)))
          (if (ans-val resp)
             (value-of-stats stats (extract-sc resp))
-            (value-of-else-block else-block (extract-sc resp))
-            )
-         )
-       )
-      )
-    )
-  )
+            (value-of-else-block else-block (extract-sc resp))))))))
+            
+         
+       
+      
+    
+  
 
+; else block
 (define value-of-else-block
   (lambda (eb scope)
     (cases else-block eb
       (a-else-block 
        (else-stats)
-       (value-of-stats else-stats sc))
-      )
-    )
-  )
+       (value-of-stats else-stats sc)))))
+      
+    
+  
 
 ; For_stmt
 (define-datatype evaluated-list evaluated-list?
@@ -216,6 +217,7 @@
    (python-list python-list?)
    (sc scope?)))
 
+; for stmt
 (define value-of-for-stmt
   (lambda (fs scope)
     (cases for-stmt fs
@@ -225,14 +227,14 @@
          (cases evaluated-list (ans-val resp)
            (a-evaluated-list 
             (python-list sc)
-            (value-of-for-body iter python-list sc sts (extract-sc resp))
-            )
-           )
-         )
-       )
-      )
-    )
-  )
+            (value-of-for-body iter python-list sc sts (extract-sc resp)))))))))
+            
+           
+         
+       
+      
+    
+  
 
 (define value-of-for-body
   (lambda (iter lst stored-sc stats sc)
@@ -242,9 +244,85 @@
               (resp2 (value-of-stats stats (extend-sc sc iter (ans-val resp1)))))
          (if (break-ans? resp2)
             (a-ans (a-none) '- (extract-sc resp2))
-            (value-of-for-body iter (cdr lst) stored-sc stats (extract-sc resp2))
-            )
-         )
-       )
-    )
-  )
+            (value-of-for-body iter (cdr lst) stored-sc stats (extract-sc resp2)))))))
+            
+         
+       
+    
+  
+
+(define value-of-expression
+ (lambda (exp scope)
+  (cases expression? exp
+   (disjunct-expression (disjunc) (value-of-disjunction disjunc scope)))))
+
+
+
+(define value-of-disjunction
+ (lambda (disj scope))
+ (cases disjunction disj
+  (a-disjunction (conjunc) (value-of-conjunction conjunc scope))
+  (cum-disjunction (disjunc conjunc) (let ((ans1 (value-of-disjunction disjunc scope)))
+                                      (let ((ans2 (value-of-conjunction conjunc (extract-sc ans1))))
+                                       (a-ans (or (ans-val ans1) (ans-val ans2)) '- (extract-sc ans2)))))))
+
+
+(define value-of-conjunction
+ (lambda (conjunc scope)
+  (cases conjunction conjunc
+   (a-conjunction (invers) (value-of-inversion invers scope))
+   (cum-conjunction (conjunc invers) (let ((ans1 (value-of-conjunction conjunc scope)))
+                                      (let ((ans2 (value-of-inversion invers (extract-sc ans1))))
+                                       (a-ans (and (ans-val ans1) (ans-val ans2)) '- (extract-sc ans2))))))))
+
+
+
+(define value-of-inversion 
+ (lambda (ex scope)
+  (cases inversion ex
+   (not-of-inversion (invers) (let ((ans (value-of-inversion invers scope)))
+                               (a-ans (not (ans-val ans)) '- (extract-sc ans))))
+   (a-comparison (comparsion) (value-of-comparison comparsion scope)))))
+
+
+(define-datatype cmp-answer cmp-answer?
+  (a-cmp-answer
+   (result boolean?)
+   (scope scope?)))   
+
+(define value-of-comparison
+ (lambda (ex scope)
+  (cases comparison ex
+    (sum-comp (sum) (value-of-sum sum scope))
+    (eq-comp (eq-sum
+                (let ((cmp-ans (value-of-eq-sum eq-sum scope)))
+                  (cases cmp-answer cmp-ans
+                    (a-cmp-answer (res sc) (a-ans res '- sc))))))
+    (lt-comp (lt-sum
+              (let ((cmp-ans (value-of-lt-sum lt-sum scope)))
+                (cases cmp-answer cmp-ans
+                  (a-cmp-answer (res sc) (a-ans res '- sc))))))
+    (gt-comp (gt-sum
+              (let ((cmp-ans (value-of-gt-sum gt-sum scope)))
+                (cases cmp-answer cmp-ans
+                  (a-cmp-answer (res sc) (a-ans res '- sc)))))))))
+
+
+(define value-of-eq-sum
+  (lambda (ex scope)
+    (cases eq-sum ex
+      (a-eq-sum (sum1 sum2)
+                (let ((ans1 (value-of-sum sum1 scope))
+                      (ans2 (value-of-sum sum2 scope)))
+                  (a-cmp-answer (= (ans-val ans1) (ans-val ans2)) (extract-sc ans2)))))))
+
+
+(define value-of-lt-sum
+  (lambda (ex scope)
+    (cases lt-sum ex
+      (a-lt-sum (sum1 sum2)
+                (let ((ans1 (value-of-sum sum1 scope))
+                      (ans2 (value-of-sum sum2 scope)))
+                  (a-cmp-answer (< (ans-val ans1) (ans-val ans2)) (extract-sc ans2)))))))
+
+
